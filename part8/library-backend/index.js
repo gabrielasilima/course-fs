c101568a7b6f43d42484f9789d516f74fb229efb
gabrielasilima <gabriela.limaifpr@gmail.com>
2026-03-31 17:32:54 -0300

    Finalizando parte 8

---
+++ part8/library-backend/index.js
@@ -0,0 +1,38 @@
+const { ApolloServer } = require('@apollo/server')
+const { startStandaloneServer } = require('@apollo/server/standalone')
+
+const typeDefs = `#graphql
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+  }
+
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
+  type Query {
+    bookCount: Int!
+    authorCount: Int!
+    allBooks(author: String, genre: String): [Book!]!
+    allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!,
+      author: String!,
+      published: Int!,
+      genres: [String!]!
+    ): Book
+    editAuthor(
+      name: String!,
+      setBornTo: Int!
+    ): Author
+  }
+`
